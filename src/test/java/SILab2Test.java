eee3efda4a30aa3f7131dc26edde367a070b3eae
unknown <lapevskidarko>
2021-05-26 22:30:57 +0200

    Initial commit

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+    private List<Integer> createList(Integer... elems) {
+        return new ArrayList<>(Arrays.asList(elems));
+    }
+
+    @Test
+    public void multipleConditionsTest() {
+        List<Time> times = new ArrayList<>(){};
+        Time time = new Time(-1, 20, 5);
+        times.add(time);
+        RuntimeException ex;
+        ex= assertThrows(RuntimeException.class, () -> SILab2.function(times));
+        assertTrue(ex.getMessage().contains("The hours are smaller than the minimum"));
+    }
+
+    @Test
+    public void everyBranchTest(){
+        List<Time> times = new ArrayList<>();
+        Time time = new Time(22, 11, 38);
+        times.add(time);
+        List<Integer> expectedResults = new ArrayList<>();
+        expectedResults.add(79898);
+        assertEquals(expectedResults, SILab2.function(times));
+    }
+}
